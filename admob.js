2d6855d62845df8c1af2ea54ef9bbc33d2bf5f59
GARVECOF <luisramongarciagelvez522@gmail.com>
2026-07-11 20:06:37 -0500

    Create admob.js

---
+++ admob.js
@@ -0,0 +1,6 @@
+// Configuración de AdMob para Garvécof
+console.log("AdMob cargado correctamente");
+
+// ID de Aplicación: ca-app-pub-6866054031665473~4209446541
+// ID del Banner: ca-app-pub-6866054031665473/4136304678
+// ID del Anuncio Bonificado: ca-app-pub-6866054031665473/8901546198
